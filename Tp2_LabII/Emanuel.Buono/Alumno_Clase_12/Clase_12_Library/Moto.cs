974fc6b0e668f349cd2dde4ea378fdd7a3fb3624
emanuell2 <emanuel.9178@gmail.com>
2016-10-07 21:10:20 -0300

    Tp2 Laboratorio2

---
+++ Tp2_LabII/Emanuel.Buono/Alumno_Clase_12/Clase_12_Library/Moto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Clase_12_Library
+{
+    public class Moto:Vehiculo
+    {
+        public Moto(Clase_12_Library.Vehiculo.EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        /// <summary>
+        /// Las motos tienen 2 ruedas
+        /// </summary>
+        protected override short CantidadRuedas
+        {
+            get
+            {
+                return 2;
+            }
+            set
+            {
+
+            }
+        }
+
+        public sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("MOTO");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("RUEDAS : {0}"+ this.CantidadRuedas.ToString());
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
